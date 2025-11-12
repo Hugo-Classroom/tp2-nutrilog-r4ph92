d3246fc71a8b458fbef28683a07bab39f93915ed
Hugo Larochelle <55212841+KookyEddie@users.noreply.github.com>
2025-11-11 21:52:42 -0500

    Delete tp2-nutrilog-r4ph92 directory

--- tp2-nutrilog-r4ph92/NutriLog/Mocks/MockData+FoodEntry.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension MockData {
-    static let foodEntries: [FoodEntry] = [
-       // TODO: Faites-vous des mocks pour les repas en utilisant les aliments!
-    ]
-}
--- tp2-nutrilog-r4ph92/NutriLog/Models/MealType.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-enum MealType: String, CaseIterable, Identifiable, Codable {
-    case breakfast = "Déjeuner"
-    case lunch = "Dîner"
-    case dinner = "Souper"
-    
-    var id: String { rawValue }
-}
